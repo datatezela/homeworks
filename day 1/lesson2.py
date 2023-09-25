df1ee9fc24f002d7390509f3f72a257e73001de3
datatezela <datatezelashvili8@gmail.com>
2023-09-25 17:59:41 +0400

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,17 @@
+name= "data "
+# name-ცვლადი
+# = არის ცვლადის მნიშვნელობის მიმნიჭებელი
+# "data" - ცვლადის მნიშვნელობა
+surname= "tezelashvili "
+age= "15 "
+height= "182 "
+
+
+
+print(name + " " + surname + " " + age + " " + height)
+
+
+
+
+
+
