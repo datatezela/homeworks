9b93af34a38d5e72363e32e8b335183c66a64126
datatezela <datatezelashvili8@gmail.com>
2023-09-28 16:50:06 +0400

    fourth commit

---
+++ day 2/homework/დავალება 1.py
@@ -0,0 +1,16 @@
+#create variables from ages of your family members
+my_age=15
+bro_age=8
+mom_age=41
+dad_age=40
+grandma_age=63
+print("my age")
+print(my_age)
+print("bro's age")
+print(bro_age)
+print("mom's age")
+print(mom_age)
+print("dad's age")
+print(dad_age)
+print("grandma's age")
+print(grandma_age)
