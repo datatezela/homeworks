2d3f2f97910f617e0e6a68992757cb8d492bc98c
datatezela <datatezelashvili8@gmail.com>
2023-10-09 21:07:44 +0400

    commit

--- project_1/calculator.py
+++ project_1/calculator.py
@@ -1,13 +1,13 @@
 print("Your numbers are:")
 x = float(input("First number = "))
-y = float(input("Second number = "))
+y = float(input("Second number/Degree = "))
 
 print("Select operation:")
 print("1. Addition")
 print("2. Substraction")
 print("3. Multiplication")
 print("4. Division")
-print("5. Exponentiation")
+print("5. Exponentiation/root")
 choice = input("1/2/3/4/5 - ")
 
 if choice == "1":
